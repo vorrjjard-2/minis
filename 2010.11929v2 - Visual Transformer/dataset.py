064fff57305e0fd1900aeeb9cd95e9bb0b128069
vorrjjard <ruben.saulog@gmail.com>
2025-12-16 10:47:55 +0800

    added augments

--- 2010.11929v2 - Visual Transformer/dataset.py
+++ 2010.11929v2 - Visual Transformer/dataset.py
@@ -7,13 +7,26 @@ import torch.nn as nn
 
 from utils import pre_index
 
+import albumentations as A 
+from albumentations.pytorch import ToTensorV2
+
+
+train_transform = A.Compose([
+    A.Resize(400, 400),
+    ToTensorV2()
+
+])
+
+
+
 class MiniDataset(Dataset):
-    def __init__(self, split):
+    def __init__(self, split, transform):
         self.split = split
         self.id_annotations, self.id_images, self.id_categories = pre_index(
             os.path.join('datasets', 'coco_dataset', self.split, '_annotations.coco.json')
         )
         self.targets = {img_id: v[0][0] for img_id, v in self.id_annotations.items()}
+        self.transform = transform
 
     def __getitem__(self, index):
         """
@@ -25,12 +38,15 @@ class MiniDataset(Dataset):
         image_bgr = cv2.imread(os.path.join('datasets', 'coco_dataset', self.split, raw_path))
         image = np.array(cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)) # type: ignore
 
+        if self.transform :
+            image = self.transform(image=image)['image']
+            
         target = self.targets[index]
         return (image, target)
 
 
 if __name__ == "__main__":
-    dataset = MiniDataset('train')
+    dataset = MiniDataset('train', train_transform)
     assert dataset[0][0].ndim == 3 and type(dataset[0][1]) == int
     print('All Tests Passed!')
 
--- 2010.11929v2 - Visual Transformer/model.py
+++ 2010.11929v2 - Visual Transformer/model.py
@@ -1,3 +1,6 @@
-class Model():
-    def __init__(self):
-        self.hello = "hi"
+import numpy as np
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+import cv2
+import torch.nn as nn
