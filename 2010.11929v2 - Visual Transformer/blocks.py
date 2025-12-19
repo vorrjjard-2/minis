55f92d119fa8d5d08943b5ede6de2dc566254553
vorrjjard <ruben.saulog@gmail.com>
2025-12-19 18:46:09 +0800

    finished model

--- 2010.11929v2 - Visual Transformer/blocks.py
+++ 2010.11929v2 - Visual Transformer/blocks.py
@@ -5,6 +5,11 @@ from torch.utils.data import Dataset, DataLoader
 import cv2
 import torch.nn as nn
 import pytorch_lightning as pl 
+import einops
+from einops import rearrange
+import math 
+
+import torch.nn.functional as F
 
 class PatchEmbedding(nn.Module):
     def __init__(self, sizes=[256, 256], S=16, d_model=1024):
@@ -25,33 +30,88 @@ class PatchEmbedding(nn.Module):
 
         B, C, H, W = x.shape
 
-        x = x.reshape(B, C, H // P, P, W // P, P)
-        x = x.permute(0, 2, 4, 1, 3, 5) 
-        x = x.reshape(B, (H // P) * (W // P), C * P * P)
+        x = rearrange(x, 'b c (hp p1) (wp p2) -> b (hp wp) (c p1 p2)', p1=P, p2=P)
         x = self.proj(x)
         return x
     
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads=8, d_model=1024):
+        super().__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+        self.scaler = math.sqrt(self.d_model // self.n_heads)
+        self.QKV = nn.Linear(self.d_model, 3 * self.d_model)
+        self.proj = nn.Linear(self.d_model, self.d_model)
+
+    def forward(self, x):
+        qkv = self.QKV(x) # B, P, d_model * 3
+        Q, K, V = torch.chunk(qkv, 3, dim=-1) # B, P, d_model (per tensor)
+
+        Q = rearrange(Q, 'b p (h x) -> b h p x', h=self.n_heads) # B, H, p, x ()
+        K = rearrange(K, 'b p (h x) -> b h p x', h=self.n_heads) # B, H, p, x ()
+        V = rearrange(V, 'b p (h x) -> b h p x', h=self.n_heads) # B, H, p, x ()
+
+        head_attns = torch.matmul(F.softmax(torch.matmul(Q, K.transpose(-2, -1)) / self.scaler, dim=-1), V)
+        attn_score = rearrange(head_attns, 'b h t d -> b t (h d)')
+
+        y = self.proj(attn_score)
+
+        return attn_score
+    
+class MLP(nn.Module):
+    def __init__(self, sizes=[1024, 1024 * 4, 1024]):
+        super().__init__()
+        self.sizes = sizes
+        self.layers = nn.ModuleList([
+            nn.Linear(self.sizes[0], self.sizes[1]),
+            nn.GELU(),
+            nn.Linear(self.sizes[1], self.sizes[2])
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+class EncoderBlock(nn.Module):
+    def __init__(self, d_model=1024):
+        super().__init__()
+        self.d_model = d_model
+
+        self.attn = MultiHeadAttention()
+        self.mlp = MLP()
+        self.ln1 = nn.LayerNorm(self.d_model)
+        self.ln2 = nn.LayerNorm(self.d_model)
+
+    def forward(self, x):
+        x = self.ln1(x)
+        x = self.attn(x) + x 
+
+        x = self.ln2(x) 
+        x = self.mlp(x)
+
+        return x
 
 
-
-
-
-
-
-
-
-
-
-
+class TransformerEncoder(nn.Module):
+    def __init__(self, N, d_model=1024):
+        super().__init__()
+        self.N = N
+        self.d_model = d_model
+        self.layers = nn.ModuleList([
+            EncoderBlock(self.d_model) for i in range(self.N)
+            ])
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
 
 
 if __name__ == "__main__":
-    patcher = PatchEmbedding()
-    img = torch.zeros((1, 3, 256, 256))
+    enc = TransformerEncoder(4)
+    x = torch.ones(3, 257, 1024)
 
-    res = patcher.forward(img)
-    print(res.shape)
-
-
-
+    y = enc.forward(x)
+    print(y.shape)
--- 2010.11929v2 - Visual Transformer/model.py
+++ 2010.11929v2 - Visual Transformer/model.py
@@ -1,4 +1,4 @@
-from blocks import PatchEmbedding
+from blocks import PatchEmbedding, EncoderBlock
 
 import numpy as np
 import os
@@ -7,18 +7,28 @@ from torch.utils.data import Dataset, DataLoader
 import cv2
 import torch.nn as nn
 import pytorch_lightning as pl 
+import torch.optim as optim
 
 class ViT(nn.Module):
-    def __init__(self, sizes=[256, 256], S=16, d_model=1024):
+    def __init__(self, sizes=[256, 256], S=16, d_model=1024, N=12, num_classes=80):
         super().__init__()
         self.sizes = sizes
         self.S = S
+        self.N = N
         self.d_model = d_model
         self.n_tokens = self.S ** 2
+        self.num_classes = num_classes
 
         self.patcher = PatchEmbedding(self.sizes, self.S, self.d_model)
         self.cls_token = nn.Parameter(torch.zeros(1, 1, self.d_model))
         self.pos_embed = nn.Parameter(torch.zeros(1, self.n_tokens + 1, self.d_model)) # + 1 to accomodate cls token
+        self.ln = nn.LayerNorm(self.d_model)
+
+        self.blocks = nn.ModuleList([
+            EncoderBlock(self.d_model) for i in range(self.N)
+        ])
+
+        self.head = nn.Linear(d_model, num_classes)
 
     def forward(self, x):
         B = x.shape[0]
@@ -32,21 +42,49 @@ class ViT(nn.Module):
         # 3. add pos embed
         pos_embeds = self.pos_embed.expand(B, -1 , -1) # has shape B, N+1, d_model
         x = x + pos_embeds
+
+        #4. transformer
+        for block in self.blocks:
+            x = block(x)
+
+        x = self.ln(x)
+
+        #5. extract the CLS token
+        x = x[:, 0]
+
+        #6. make classification
+        x = self.head(x)
         return x
 
 class VitWrapper(pl.LightningModule):
     def __init__(self, model):
+        super().__init__()
         self.model = model
+        self.criterion = nn.CrossEntropyLoss()
     
     def forward(self, x):
         return self.model(x)
-    
-    def training_step(self, x):
-        x = self.forward(x)
 
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
 
+        loss = self.criterion(y, logits)
+        preds = torch.argmax(logits, dim=1)
+        acc = (preds == y).float().mean()
+
+        self.log("train/loss", loss, prog_bar=True, on_step=True, on_epoch=True)
+        self.log("train/acc", acc, prog_bar=True, on_step=True, on_epoch=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
 if __name__ == "__main__":
     model = ViT()
     test_input = torch.zeros(3, 3, 256, 256)
     out = model(test_input)
+
+    print(out.shape)
